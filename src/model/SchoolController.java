0335368e92d075e6dff7624ec63a63031c7c34e4
Jhoan <arturomarinmaigual@gmail.com>
2025-10-26 23:24:38 -0500

    final version

--- src/model/SchoolController.java
+++ src/model/SchoolController.java
@@ -27,6 +27,7 @@ public class SchoolController {
         for (int i = 0; i<floors; i++){
             for (int j = 0; j<col; j++){
                 if (edificio[i][j] != null && edificio[i][j].getSerialnumber().equals(serialNumber)){
+                    System.out.println("Ya existe un computador con ese serial.");
                     return false;
                 }
             }
@@ -34,11 +35,15 @@ public class SchoolController {
         for (int j =0 ; j<col; j++){
             if (edificio[floor][j]==null){
                 edificio[floor][j] = new Computer (serialNumber, floor, j);
+                System.out.println("Computador agregado en piso " + floor + ", columna " + j);
                 return true;
             }
         }
+        System.out.println("No hay espacio disponible en el piso " + floor);
         return false;
     }
+    
+
 
 
     public boolean agregarIncidenteEnComputador(String serial, Incident inc) {
@@ -53,6 +58,25 @@ public class SchoolController {
         }
         return false;
 
+    }
+    public Computer consultarComputadorConMasIncidentes() {
+        Computer topComputer = null;
+        int maxIncidents = -1;
+        for (int i =0; i<floors; i++){
+            for (int j=0;j<col; j++){
+                if (edificio[i][j]!=null){
+                    int count = edificio[i][j].getIncidentCount();
+                    if (count > maxIncidents){
+                        maxIncidents = count;
+                        topComputer = edificio[i][j];
+                    }
+                }
+            }
+        }
+
+       
+
+        return topComputer;
     }
 
     public void getComputerList() {
